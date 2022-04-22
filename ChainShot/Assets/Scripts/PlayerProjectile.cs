c2987bc256be9c700e2d1c54940b0df202aec0a5
SeanBrennan645 <seanbrennan645@gmail.com>
2022-04-22 19:38:56 +0100

    Added basic projectile launcher

---
+++ ChainShot/Assets/Scripts/PlayerProjectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectile : MonoBehaviour
+{
+    [SerializeField] GameObject Projectile;
+    [SerializeField] float speed = 15.0f;
+
+    private bool fired = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fired = true;
+        }
+
+        if(fired)
+        {
+            launchProjectile();
+        }
+    }
+
+    void launchProjectile()
+    {
+        Projectile.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
