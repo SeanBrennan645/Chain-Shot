43c314e2f016fd488e346d7a51af37da9bcfde57
SeanBrennan645 <seanbrennan645@gmail.com>
2022-04-23 12:51:35 +0100

    Added Basic Object Pool to spawn in enemies

---
+++ ChainShot/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    private bool isHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("I've been hit!!");
+            this.enabled = false;
+        }
+        else
+            return;
+    }
+}
---
+++ ChainShot/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+    [SerializeField] Transform SpawnLocation;
+    [SerializeField] int poolSize = 5;
+    [SerializeField] float spawnTimer = 1.0f;
+
+    private GameObject[] pool;
+
+    private void Awake()
+    {
+        PopulatePool();
+    }
+
+    private void PopulatePool()
+    {
+        pool = new GameObject[poolSize];
+
+        for(int i = 0; i<pool.Length; i++)
+        {
+            pool[i] = Instantiate(enemy, SpawnLocation);
+            pool[i].SetActive(false);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while(true)
+        {
+            EnableObjectInPool();
+            yield return new WaitForSeconds(spawnTimer);
+        }
+    }
+
+    private void EnableObjectInPool()
+    {
+        for(int i = 0; i<pool.Length; i++)
+        {
+            if(pool[i].activeInHierarchy == false)
+            {
+                pool[i].SetActive(true);
+                return;
+            }
+        }
+    }
+}
